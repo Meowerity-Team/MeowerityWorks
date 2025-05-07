4e5f1afe18c733f65cc5328b25a9f65d783b7ac1
MeowertyK <meowerityk@outlook.com>
2025-05-07 14:24:38 +0900

    Update load.lua

--- mgm/gamemode/load.lua
+++ mgm/gamemode/load.lua
@@ -1,11 +1,2 @@
-version = 0.1
-print("-----------------------------------------")
-print("Meowerity Works alpha " version)
-print("By Meowerity")
-print("-----------------------------------------")
-print("--!--!--!--!--!--!--!--!--!--!--!--!--!-- Meowerity CUI")
-dinfo = [[Beta Testing Version
-So as i can see you need the MB key
-please check is it RED-2222-MMMM]]
-print( dinfo )
-print("--!--!--!--!--!--!--!--!--!--!--!--!--!--")
+versionmworks = 0.1
+print ("hello and thank you for using Meowerity Works")
